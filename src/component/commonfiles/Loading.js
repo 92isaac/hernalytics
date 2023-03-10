db95e3d33afa7bc53d54d7c05aab4aaeb54abc6e
Sangodare Isaac <sangodareisaac92@gmail.com>
2023-03-10 13:12:59 +0100

    fix bug

--- src/component/commonfiles/Loading.js
+++ src/component/commonfiles/Loading.js
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="flex justify-center items-center h-40">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-4 border-red-900"></div>
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-4 border-white-900"></div>
     </div>
   );
 };
--- src/routes/CheckResult.js
+++ src/routes/CheckResult.js
@@ -1,13 +1,13 @@
 import React from 'react'
-// import D3Demo from '../component/commonfiles/D3Demo'
+import D3Demo from '../component/commonfiles/D3Demo'
 import Nigeriamap from '../component/commonfiles/Nigeriamap'
 
 const CheckResult = () => {
   return (
     <div className='body'>
       sgsggs
-      {/* <Nigeriamap /> */}
-    {/* <D3Demo /> */}
+      <Nigeriamap />
+    <D3Demo />
     </div>
   )
 }
